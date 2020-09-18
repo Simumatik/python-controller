8b6e4bc42bb5d888c31ad7d9edc7230d8f4ee782
ahugoson <aron.hugoson@simumatik.com>
2020-09-18 15:32:37 +0200

    Simplified example code

--- example.py
+++ example.py
@@ -25,18 +25,10 @@ if __name__ == '__main__':
     while True:
         updateInputs()
 
-        # You can  handle the inputs, and manipulate the outputs
+        # You can handle the inputs here, and manipulate the outputs
         # Example: connect out_0 directly to in_0
         outputs[0] = inputs[0]
 
-        # We can invert in_1 and output to out_1
-        outputs[1] = not inputs[1]
-
-        # If both in_0 and in_1 are active, activate out_2
-        if inputs[0] and inputs[1]:
-            outputs[2] = 1
-
         updateOutputs()
-        print("I: " + str(inputs) + " O: " + str(outputs), end="\r")
         time.sleep(1e-5)
         
