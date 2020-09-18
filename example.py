eb52a62c629329c04b8b6fdead4cfa9529bd51fc
ahugoson <aron.hugoson@simumatik.com>
2020-09-18 13:57:46 +0200

    Added functions for updating IO

--- example.py
+++ example.py
@@ -8,10 +8,29 @@ if __name__ == '__main__':
     _controller.addVariable("inputs", "byte", 0)
     _controller.addVariable("outputs", "byte", 0)
     _controller.start()
+    inputs = [0, 0, 0, 0, 0, 0, 0, 0]
+    outputs = [0, 0, 0, 0, 0, 0, 0, 0]
+    input_byte = 0
+    output_byte = 0
+
+    def updateInputs():
+        input_byte = _controller.getValue("inputs")
+        for i in range(8):
+            inputs[i] = 1 if input_byte & (2 ** i) else 0
+
+    def updateOutputs():
+        output_byte = 0
+        for i in range(8):
+            output_byte += 2 ** i if outputs[i] == 1 else 0
+        _controller.setValue("outputs", output_byte)
 
     while True:
+        updateInputs()
 
-        value = _controller.getValue("inputs")
-        _controller.setValue("outputs", value)
-        
+        # Here you handle the inputs, and manipulate the outputs
+        outputs = inputs
+
+        updateOutputs()
+        print("I: " + str(inputs) + " O: " + str(outputs), end="\r")
         time.sleep(1e-5)
+        
